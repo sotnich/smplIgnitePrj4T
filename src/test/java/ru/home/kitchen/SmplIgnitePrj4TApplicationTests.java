9de90b6c563a4a37b3b5e988f33a572e9e32069c
boris <zhguchev@gmail.com>
2016-11-01 00:42:19 +0300

    Initial commit

---
+++ src/test/java/ru/home/kitchen/SmplIgnitePrj4TApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.home.kitchen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmplIgnitePrj4TApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
